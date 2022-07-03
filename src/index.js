7573cc59f775ff8523580157767d0366628bee68
simo-seven <88766651+simo-seven@users.noreply.github.com>
2022-07-03 17:38:22 +0200

    someText

--- src/index.js
+++ src/index.js
@@ -9,3 +9,26 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ✅ Works
+async function getUser() {
+  try {
+    const response = await fetch('https://randomuser.me/api/', {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getUser();
